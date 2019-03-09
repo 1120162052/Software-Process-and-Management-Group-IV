2719fa63018fd65f5ac5f217fe97c2d4a7c3d695
wyj <531442894@qq.com>
2019-03-09 16:20:21 +0800

    ...

---
+++ src/main/java/team/abc/ssm/common/persistence/entity/DataEntity.java
@@ -0,0 +1,65 @@
+package team.abc.ssm.common.persistence.entity;
+
+import java.util.Date;
+
+public class DataEntity<T> extends BaseEntity<T> {
+
+    protected int commonStatus;             // 0或null - 正常, 1 - 删除
+    protected String commonCreateUserId;    // 创建者id
+    protected Date commonCreateDate;        // 创建日期
+    protected String commonModifyUserId;    // 最后修改者id
+    protected Date commonModifyDate;        // 最后修改日期
+    protected String commonRemarks;         // 备注
+
+    public DataEntity() {
+
+    }
+
+    public int getCommonStatus() {
+        return commonStatus;
+    }
+
+    public void setCommonStatus(int commonStatus) {
+        this.commonStatus = commonStatus;
+    }
+
+    public String getCommonCreateUserId() {
+        return commonCreateUserId;
+    }
+
+    public void setCommonCreateUserId(String commonCreateUserId) {
+        this.commonCreateUserId = commonCreateUserId;
+    }
+
+    public Date getCommonCreateDate() {
+        return commonCreateDate;
+    }
+
+    public void setCommonCreateDate(Date commonCreateDate) {
+        this.commonCreateDate = commonCreateDate;
+    }
+
+    public String getCommonModifyUserId() {
+        return commonModifyUserId;
+    }
+
+    public void setCommonModifyUserId(String commonModifyUserId) {
+        this.commonModifyUserId = commonModifyUserId;
+    }
+
+    public Date getCommonModifyDate() {
+        return commonModifyDate;
+    }
+
+    public void setCommonModifyDate(Date commonModifyDate) {
+        this.commonModifyDate = commonModifyDate;
+    }
+
+    public String getCommonRemarks() {
+        return commonRemarks;
+    }
+
+    public void setCommonRemarks(String commonRemarks) {
+        this.commonRemarks = commonRemarks;
+    }
+}
